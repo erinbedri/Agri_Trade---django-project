5b633b1aa0e163a2f95e8c32888e32ef23533309
erinbedri <erinbedri@gmail.com>
2022-10-25 09:49:14 +0200

    registration and login func

--- agri_trade/accounts/forms.py
+++ agri_trade/accounts/forms.py
@@ -1,5 +1,9 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django import forms
+from django.core.exceptions import ValidationError
+
+UserModel = get_user_model()
 
 
 class CustomAuthenticationForm(AuthenticationForm):
@@ -10,3 +14,66 @@ class CustomAuthenticationForm(AuthenticationForm):
                                widget=forms.PasswordInput(attrs={'placeholder': 'Password',
                                                                  'class': 'form-control',
                                                                  }))
+
+    class Meta:
+        model = UserModel
+
+
+class CustomRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(required=True,
+                                 max_length=30,
+                                 widget=forms.TextInput(attrs={'placeholder': 'First Name',
+                                                               'class': 'form-control',
+                                                               }))
+    last_name = forms.CharField(required=True,
+                                max_length=30,
+                                widget=forms.TextInput(attrs={'placeholder': 'Last Name',
+                                                              'class': 'form-control',
+                                                              }))
+    email = forms.EmailField(required=True,
+                             widget=forms.TextInput(attrs={'placeholder': 'Email Address',
+                                                           'class': 'form-control',
+                                                           }))
+    username = forms.CharField(required=True,
+                               max_length=30,
+                               widget=forms.TextInput(attrs={'placeholder': 'Username',
+                                                             'class': 'form-control',
+                                                             }))
+    password1 = forms.CharField(required=True,
+                                widget=forms.PasswordInput(attrs={'placeholder': 'Password',
+                                                                  'class': 'form-control',
+                                                                  'data-toggle': 'password',
+                                                                  'id': 'password',
+                                                                  }))
+    password2 = forms.CharField(required=True,
+                                widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password',
+                                                                  'class': 'form-control',
+                                                                  'data-toggle': 'password',
+                                                                  'id': 'password',
+                                                                  }))
+
+    class Meta:
+        model = UserModel
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(CustomRegistrationForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+        return user
+
+    def clean(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+
+        if UserModel.objects.filter(email=email):
+            raise ValidationError('Email already exists!')
+
+        if UserModel.objects.filter(username=username):
+            raise ValidationError('Username already exists!')
+
+        return self.cleaned_data
--- agri_trade/accounts/views.py
+++ agri_trade/accounts/views.py
@@ -1,7 +1,8 @@
-from django.contrib.auth import get_user_model, authenticate, login
+from django.contrib.auth import get_user_model, authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-from agri_trade.accounts.forms import CustomAuthenticationForm
+from agri_trade.accounts.forms import CustomAuthenticationForm, CustomRegistrationForm
 
 UserModel = get_user_model()
 
@@ -29,17 +30,27 @@ def login_user(request):
     return render(request, 'accounts/login.html', context)
 
 
+@login_required
 def logout_user(request):
-    context = {
-
-    }
-
-    return render(request, 'accounts/logout.html', context)
+    logout(request)
+    return redirect('web:homepage')
 
 
 def register_user(request):
-    context = {
+    if request.user.is_authenticated:
+        return redirect('web:homepage')
 
+    if request.method == 'POST':
+        form = CustomRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('web:homepage')
+    else:
+        form = CustomRegistrationForm()
+
+    context = {
+        'form': form,
     }
 
     return render(request, 'accounts/register.html', context)
