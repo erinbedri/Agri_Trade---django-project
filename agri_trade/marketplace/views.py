a8945a406d9eaec065a0e8474b9d2bb55241237b
erinbedri <erinbedri@gmail.com>
2022-10-25 17:02:15 +0200

    search and filter func implemented

--- agri_trade/marketplace/views.py
+++ agri_trade/marketplace/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 
 from agri_trade.marketplace.models import Product
@@ -6,10 +7,32 @@ from agri_trade.marketplace.models import Product
 
 @login_required
 def marketplace(request):
-    products = Product.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+
+    products = Product.objects \
+        .filter(
+            Q(category__icontains=q) |
+            Q(cultivation_type__icontains=q) |
+            Q(origin__icontains=q) |
+            Q(location__icontains=q) |
+            Q(variety__icontains=q) |
+            Q(type__icontains=q) |
+            Q(description__icontains=q) |
+            Q(form__icontains=q) |
+            Q(owner__username=q)) \
+        .order_by('-created_on')
+
+    categories = {product.category for product in Product.objects.all()}
+    cultivation_types = {product.cultivation_type for product in Product.objects.all()}
+    origins = {product.origin for product in Product.objects.all()}
+    locations = {product.location for product in Product.objects.all()}
 
     context = {
         'products': products,
+        'categories': categories,
+        'cultivation_types': cultivation_types,
+        'origins': origins,
+        'locations': locations,
     }
 
     return render(request, 'marketplace/marketplace.html', context)
