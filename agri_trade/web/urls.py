f115e6a884396623b4d3a74a5f43478c0391a690
erinbedri <erinbedri@gmail.com>
2022-11-09 13:15:55 +0100

    added about, imprint, t&c, and data protection templates

--- agri_trade/web/urls.py
+++ agri_trade/web/urls.py
@@ -4,4 +4,8 @@ from agri_trade.web import views
 
 urlpatterns = [
     path('', views.show_homepage, name='homepage'),
+    path('about/', views.about, name='about'),
+    path('imprint/', views.imprint, name='imprint'),
+    path('terms-and-conditions/', views.terms_and_conditions, name='terms and conditions'),
+    path('data-protection/', views.data_protection, name='data protection'),
 ]
--- agri_trade/web/views.py
+++ agri_trade/web/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 
 
 def show_homepage(request):
-    context = {
+    return render(request, 'web/homepage.html')
 
-    }
 
-    return render(request, 'web/homepage.html', context)
+def about(request):
+    return render(request, 'web/about.html')
+
+
+def imprint(request):
+    return render(request, 'web/imprint.html')
+
+
+def terms_and_conditions(request):
+    return render(request, 'web/terms_and_conditions.html')
+
+
+def data_protection(request):
+    return render(request, 'web/data_protection.html')
+
