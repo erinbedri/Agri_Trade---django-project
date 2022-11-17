1ca0eaa811369bdf2682d44baee3b5f87c70dd5b
erinbedri <erinbedri@gmail.com>
2022-11-17 14:06:07 +0100

    changed FBV to CBV in web app

--- agri_trade/web/urls.py
+++ agri_trade/web/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from agri_trade.web import views
 
 urlpatterns = [
-    path('', views.show_homepage, name='homepage'),
-    path('about/', views.about, name='about'),
-    path('imprint/', views.imprint, name='imprint'),
-    path('terms-and-conditions/', views.terms_and_conditions, name='terms and conditions'),
-    path('data-protection/', views.data_protection, name='data protection'),
+    path('', views.HomepageView.as_view(), name='homepage'),
+    path('about/', views.AboutPageView.as_view(), name='about'),
+    path('imprint/', views.ImprintPageView.as_view(), name='imprint'),
+    path('terms-and-conditions/', views.TermsAndConditionsPageView.as_view(), name='terms and conditions'),
+    path('data-protection/', views.DataProtectionPageView.as_view(), name='data protection'),
 ]
--- agri_trade/web/views.py
+++ agri_trade/web/views.py
@@ -1,22 +1,43 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def show_homepage(request):
-    return render(request, 'web/homepage.html')
+# def show_homepage(request):
+#     return render(request, 'web/homepage.html')
 
 
-def about(request):
-    return render(request, 'web/about.html')
+# def about(request):
+#     return render(request, 'web/about.html')
+#
+#
+# def imprint(request):
+#     return render(request, 'web/imprint.html')
+#
+#
+# def terms_and_conditions(request):
+#     return render(request, 'web/terms_and_conditions.html')
+#
+#
+# def data_protection(request):
+#     return render(request, 'web/data_protection.html')
 
 
-def imprint(request):
-    return render(request, 'web/imprint.html')
+class HomepageView(TemplateView):
+    template_name = 'web/homepage.html'
 
 
-def terms_and_conditions(request):
-    return render(request, 'web/terms_and_conditions.html')
+class AboutPageView(TemplateView):
+    template_name = 'web/about.html'
 
 
-def data_protection(request):
-    return render(request, 'web/data_protection.html')
+class ImprintPageView(TemplateView):
+    template_name = 'web/imprint.html'
+
+
+class TermsAndConditionsPageView(TemplateView):
+    template_name = 'web/terms_and_conditions.html'
+
+
+class DataProtectionPageView(TemplateView):
+    template_name = 'web/data_protection.html'
 
