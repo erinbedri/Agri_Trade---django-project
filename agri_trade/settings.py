a25899c15a7c50e252985e7d7367a738e344bb2d
erinbedri <erinbedri@gmail.com>
2022-11-17 12:12:12 +0100

    migrated to postgres

--- agri_trade/settings.py
+++ agri_trade/settings.py
@@ -75,10 +75,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'agri_trade.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_agritrade',
+        'USER': 'postgres',
+        'PASSWORD': '123QWE',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
