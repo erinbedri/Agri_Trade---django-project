4802c7c66aa52cc3006677abc01a645505ad0390
erinbedri <erinbedri@gmail.com>
2022-11-19 11:56:29 +0100

    added success/error messages to view on accounts app

--- agri_trade/accounts/views.py
+++ agri_trade/accounts/views.py
@@ -4,10 +4,20 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from agri_trade.accounts.forms import CustomAuthenticationForm, CustomRegistrationForm, EditAccountForm, EditCompanyForm
-
 from agri_trade.accounts import services as accounts_services
 
 UserModel = get_user_model()
+
+LOGIN_SUCCESS_MESSAGE = 'You are now successfully logged in!'
+LOGIN_ERROR_MESSAGE = 'Login was unsuccessful! Fix the issues below.'
+
+LOGOUT_SUCCESS_MESSAGE = 'You are now successfully logged out from your account!'
+
+REGISTRATION_SUCCESS_MESSAGE = 'Your registration was successful! Please log in!'
+REGISTRATION_ERROR_MESSAGE = 'Registration was unsuccessful! Fix the issues below.'
+
+EDIT_SUCCESS_MESSAGE = 'Your account was updated successfully!'
+EDIT_ERROR_MESSAGE = 'Edit was unsuccessful! Fix the issues below.'
 
 
 def login_user(request):
@@ -22,10 +32,10 @@ def login_user(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                messages.success(request, f'You are now successfully logged in as {request.user.username}!')
+                messages.success(request, LOGIN_SUCCESS_MESSAGE)
                 return redirect('marketplace:marketplace')
         else:
-            messages.error(request, 'Login was unsuccessful! Fix the issues below.')
+            messages.error(request, LOGIN_ERROR_MESSAGE)
     else:
         form = CustomAuthenticationForm()
 
@@ -39,7 +49,7 @@ def login_user(request):
 @login_required
 def logout_user(request):
     logout(request)
-    messages.success(request, 'You are now successfully logged out from your account!')
+    messages.success(request, LOGOUT_SUCCESS_MESSAGE)
     return redirect('web:homepage')
 
 
@@ -50,11 +60,11 @@ def register_user(request):
     if request.method == 'POST':
         form = CustomRegistrationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('web:homepage')
+            form.save()
+            messages.success(request, REGISTRATION_SUCCESS_MESSAGE)
+            return redirect('accounts:login')
         else:
-            messages.error(request, 'Registration was unsuccessful! Fix the issues below.')
+            messages.error(request, REGISTRATION_ERROR_MESSAGE)
     else:
         form = CustomRegistrationForm()
 
@@ -86,10 +96,10 @@ def edit_account(request):
         if account_form.is_valid() and company_form.is_valid():
             account_form.save()
             company_form.save()
-            messages.success(request, 'Your account was updated successfully!')
+            messages.success(request, EDIT_SUCCESS_MESSAGE)
             return redirect('accounts:account')
         else:
-            messages.error(request, 'Edit was unsuccessful! Fix the issues below.')
+            messages.error(request, EDIT_ERROR_MESSAGE)
     else:
         account_form = EditAccountForm(instance=request.user)
         company_form = EditCompanyForm(instance=company)
