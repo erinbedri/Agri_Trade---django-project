49c943a8a09a996d038b258b3174a453baf30d1e
erinbedri <erinbedri@gmail.com>
2022-11-22 15:21:31 +0100

    updated settings to fix DEBUG

--- agri_trade/settings.py
+++ agri_trade/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = bool(os.environ.get('DEBUG', 'False'))
+DEBUG = bool(os.environ.get('DEBUG', 'FALSE'))
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(' ')
 
