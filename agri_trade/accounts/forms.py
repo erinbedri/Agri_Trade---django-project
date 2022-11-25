5d25bc2a4c6d6aa7b91b0b7cf68dbb14e46873c5
erinbedri <65279256+erinbedri@users.noreply.github.com>
2022-11-25 12:15:29 +0100

    added constants to accounts forms

--- agri_trade/accounts/forms.py
+++ agri_trade/accounts/forms.py
@@ -11,51 +11,93 @@ UserModel = get_user_model()
 
 
 class CustomAuthenticationForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username',
-                                                             'class': 'form-control',
-                                                             }))
-    password = forms.CharField(required=True,
-                               widget=forms.PasswordInput(attrs={'placeholder': 'Password',
-                                                                 'class': 'form-control',
-                                                                 'id': 'password',
-                                                                 }))
+    USERNAME_PLACEHOLDER = 'Username'
+    PASSWORD_PLACEHOLDER = 'Password'
+
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': USERNAME_PLACEHOLDER,
+                'class': 'form-control',
+            }))
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': PASSWORD_PLACEHOLDER,
+                'class': 'form-control',
+                'id': 'password',
+            }))
 
     class Meta:
         model = UserModel
 
 
 class CustomRegistrationForm(UserCreationForm):
-    first_name = forms.CharField(required=True,
-                                 max_length=30,
-                                 widget=forms.TextInput(attrs={'placeholder': 'First Name',
-                                                               'class': 'form-control',
-                                                               }))
-    last_name = forms.CharField(required=True,
-                                max_length=30,
-                                widget=forms.TextInput(attrs={'placeholder': 'Last Name',
-                                                              'class': 'form-control',
-                                                              }))
-    email = forms.EmailField(required=True,
-                             widget=forms.TextInput(attrs={'placeholder': 'Email Address',
-                                                           'class': 'form-control',
-                                                           }))
-    username = forms.CharField(required=True,
-                               max_length=30,
-                               widget=forms.TextInput(attrs={'placeholder': 'Username',
-                                                             'class': 'form-control',
-                                                             }))
-    password1 = forms.CharField(required=True,
-                                widget=forms.PasswordInput(attrs={'placeholder': 'Password',
-                                                                  'class': 'form-control',
-                                                                  'data-toggle': 'password',
-                                                                  'id': 'password',
-                                                                  }))
-    password2 = forms.CharField(required=True,
-                                widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password',
-                                                                  'class': 'form-control',
-                                                                  'data-toggle': 'password',
-                                                                  'id': 'password',
-                                                                  }))
+    FIRST_NAME_MAX_LENGTH = 30
+    FIRST_NAME_PLACEHOLDER = 'First Name'
+
+    LAST_NAME_MAX_LENGTH = 30
+    LAST_NAME_PLACEHOLDER = 'Last Name'
+
+    EMAIL_PLACEHOLDER = 'Email Address'
+
+    USERNAME_MAX_LENGTH = 30
+    USERNAME_PLACEHOLDER = 'Username'
+
+    PASSWORD1_PLACEHOLDER = 'Password'
+
+    PASSWORD2_PLACEHOLDER = 'Confirm Password'
+
+    first_name = forms.CharField(
+        required=True,
+        max_length=FIRST_NAME_MAX_LENGTH,
+        widget=forms.TextInput(
+            attrs={'placeholder': FIRST_NAME_PLACEHOLDER,
+                   'class': 'form-control',
+                   }))
+
+    last_name = forms.CharField(
+        required=True,
+        max_length=LAST_NAME_MAX_LENGTH,
+        widget=forms.TextInput(
+            attrs={'placeholder': LAST_NAME_PLACEHOLDER,
+                   'class': 'form-control',
+                   }))
+
+    email = forms.EmailField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={'placeholder': EMAIL_PLACEHOLDER,
+                   'class': 'form-control',
+                   }))
+
+    username = forms.CharField(
+        required=True,
+        max_length=USERNAME_MAX_LENGTH,
+        widget=forms.TextInput(
+            attrs={'placeholder': USERNAME_PLACEHOLDER,
+                   'class': 'form-control',
+                   }))
+
+    password1 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={'placeholder': PASSWORD1_PLACEHOLDER,
+                   'class': 'form-control',
+                   'data-toggle': 'password',
+                   'id': 'password',
+                   }))
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={'placeholder': PASSWORD2_PLACEHOLDER,
+                   'class': 'form-control',
+                   'data-toggle': 'password',
+                   'id': 'password',
+                   }))
 
     class Meta:
         model = UserModel
@@ -85,21 +127,38 @@ class CustomRegistrationForm(UserCreationForm):
 
 
 class EditAccountForm(forms.ModelForm):
-    first_name = forms.CharField(required=True,
-                                 max_length=30,
-                                 widget=forms.TextInput(attrs={'class': 'form-control',
-                                                               }))
-    last_name = forms.CharField(required=True,
-                                max_length=30,
-                                widget=forms.TextInput(attrs={'class': 'form-control',
-                                                              }))
-    email = forms.EmailField(required=True,
-                             widget=forms.TextInput(attrs={'class': 'form-control',
-                                                           }))
-    username = forms.CharField(required=True,
-                               max_length=30,
-                               widget=forms.TextInput(attrs={'class': 'form-control',
-                                                             }))
+    FIRST_NAME_MAX_LENGTH = 30
+
+    LAST_NAME_MAX_LENGTH = 30
+
+    USERNAME_MAX_LENGTH = 30
+
+    first_name = forms.CharField(
+        required=True,
+        max_length=FIRST_NAME_MAX_LENGTH,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',
+                   }))
+
+    last_name = forms.CharField(
+        required=True,
+        max_length=LAST_NAME_MAX_LENGTH,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',
+                   }))
+
+    email = forms.EmailField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',
+                   }))
+
+    username = forms.CharField(
+        required=True,
+        max_length=USERNAME_MAX_LENGTH,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',
+                   }))
 
     class Meta:
         model = UserModel
@@ -107,31 +166,62 @@ class EditAccountForm(forms.ModelForm):
 
 
 class EditCompanyForm(forms.ModelForm):
+    NAME_MAX_LENGTH = 100
+
+    VAT_MAX_LENGTH = 25
+
+    ADDRESS_MAX_LENGTH = 200
+
+    POSTCODE_MAX_LENGTH = 15
+
+    LOCATION_MAX_LENGTH = 25
+
+    DESCRIPTION_MAX_LENGTH = 500
+
+    DESCRIPTION_ROWS = 5
+
+    name = forms.CharField(
+        max_length=NAME_MAX_LENGTH,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',
+                   }))
+
+    vat = forms.CharField(
+        max_length=VAT_MAX_LENGTH,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',
+                   }))
+
+    address = forms.CharField(
+        max_length=ADDRESS_MAX_LENGTH,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',
+                   }))
+
+    postcode = forms.CharField(
+        max_length=POSTCODE_MAX_LENGTH,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',
+                   }))
+
+    location = forms.CharField(
+        max_length=LOCATION_MAX_LENGTH,
+        widget=forms.TextInput(
+            attrs={'class': 'form-control',
+                   }))
+
     country = CountryField().formfield(
         widget=CountrySelectWidget(
            attrs={"class": "form-control"}
         )
     )
 
-    name = forms.CharField(max_length=100,
-                           widget=forms.TextInput(attrs={'class': 'form-control',
-                                                         }))
-    vat = forms.CharField(max_length=25,
-                          widget=forms.TextInput(attrs={'class': 'form-control',
-                                                        }))
-    address = forms.CharField(max_length=200,
-                              widget=forms.TextInput(attrs={'class': 'form-control',
-                                                            }))
-    postcode = forms.CharField(max_length=15,
-                               widget=forms.TextInput(attrs={'class': 'form-control',
-                                                             }))
-    location = forms.CharField(max_length=25,
-                               widget=forms.TextInput(attrs={'class': 'form-control',
-                                                             }))
-    description = forms.CharField(max_length=500,
-                                  widget=forms.Textarea(attrs={'class': 'form-control',
-                                                               'rows': 5,
-                                                               }))
+    description = forms.CharField(
+        max_length=DESCRIPTION_MAX_LENGTH,
+        widget=forms.Textarea(
+            attrs={'class': 'form-control',
+                   'rows': DESCRIPTION_ROWS,
+                   }))
 
     class Meta:
         model = Company
